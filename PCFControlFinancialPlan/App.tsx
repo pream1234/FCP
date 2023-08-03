abf9886c8685154a8194c1cb3f8e8a1df9ebe1d1
pream <ab80rzz@mmm.com>
2023-08-03 23:57:42 +0530

    changes

--- PCFControlFinancialPlan/App.tsx
+++ PCFControlFinancialPlan/App.tsx
@@ -9,6 +9,8 @@ import { IPublicClientApplication, PopupRequest } from "@azure/msal-browser";
 import { Text } from "@fluentui/react/lib/Text";
 import PcfForm from "./UI/PcfForm";
 import "./App.css";
+import { AppProvider } from "./AppContext";
+import { Login } from "./UI/Login";
 // import { AppProvider } from "./AppContext";
 // import { Layout } from "./ui/Layout";
 // import { UserLookup } from "./components/UserLookup";
@@ -24,9 +26,20 @@ export const App: React.FC<IAppProps> = (props: IAppProps) => {
 
   return (
     <div className="App">
-      <MsalProvider instance={msalInstance}>
-        <PcfForm />
-      </MsalProvider>
+      <AppProvider
+        componentContext={componentContext}
+        tokenRequest={tokenRequest}
+      >
+        <MsalProvider instance={msalInstance}>
+          <Login />
+          <AuthenticatedTemplate>
+            <PcfForm />
+          </AuthenticatedTemplate>
+          <UnauthenticatedTemplate>
+            <Text>Please use the Sign in button to login.</Text>
+          </UnauthenticatedTemplate>
+        </MsalProvider>
+      </AppProvider>
     </div>
   );
 };
